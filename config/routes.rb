504c85ec6eedb8d318734a1b1c2f90ae300d6b6e
Mai Yuyan <maiyuyan@Mais-MacBook-Pro.local>
2023-03-16 16:50:18 +0800

    prof q dont kill fishball

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks do
-  resource :subtasks
+  resources :subtasks
   end
   #creates all 7 routes for you
   root "tasks#index"
